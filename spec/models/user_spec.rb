80901eb69f7e1f912d3067db776bf2f73356b0e4
scribblas <scribblas@gmail.com>
2012-09-23 20:30:08 +0300

    Finish user signup

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -66,6 +66,16 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "email with mixed case" do
+    let(:mixed_case_email) { "kEIsTaS@mAILas.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      @user.reload.email.should == mixed_case_email.downcase
+    end
+  end
+
   describe "when password is not present" do
     before { @user.password = @user.password_confirmation = " " }
     it { should_not be_valid }
