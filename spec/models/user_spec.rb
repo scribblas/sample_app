0b3c08d212f8013930af96ba0e371542f103cc26
scribblas <scribblas@gmail.com>
2012-10-03 13:39:31 +0300

    Finish sign in

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -20,6 +20,8 @@ describe User do
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
   it { should respond_to(:authenticate) }
+  it { should respond_to(:remember_token) }
+
   it { should be_valid }
 
   describe "when name is not present" do
@@ -110,5 +112,10 @@ describe User do
       before { @user.password = @user.password_confirmation = "a" * 5 }
       it { should be_invalid }
     end
+  end
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not }
   end
 end
--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -18,21 +18,90 @@ describe "User Pages" do
 
     before { visit signup_path }
     let(:submit) { "Create my account" }
+
     describe "with invalid information" do
+
+      describe "after submission" do
+        before { click_button submit }
+
+        it { should have_selector("title", text: "Sign up") }
+        it { should have_content("error") }
+      end
+
+      describe "when password is less than 6 characters" do
+        before do
+          fill_in "Name",         with: "Example User"
+          fill_in "Email",        with: "example@user.com"
+          fill_in "Password",     with: "foob1"
+          fill_in "Confirmation", with: "foob1"        
+          click_button submit
+        end
+
+        let(:str) { "Password is too short (minimum is 6 characters)" }
+        it { should have_selector("title", text: "Sign up") }
+        it { should have_content(str) }
+      end
+
+      describe "when password & confirmation mismatch" do
+        before do
+          fill_in "Name",         with: "Example User"
+          fill_in "Email",        with: "example@user.com"
+          fill_in "Password",     with: "foobar1"
+          fill_in "Confirmation", with: "fooar1" 
+          click_button submit
+        end
+
+        let(:str) { "Password doesn't match confirmation" }
+        it { should have_selector("title", text: "Sign up") }
+        it { should have_content(str) }
+      end
+
+
+      describe "when email format is invalid" do
+        before do
+          fill_in "Name",         with: "Example User"
+          fill_in "Email",        with: "exampleuser.com"
+          fill_in "Password",     with: "foob1"
+          fill_in "Confirmation", with: "foob1"        
+          click_button submit
+        end
+
+        let(:str) { "Email is invalid" }
+        it { should have_selector("title", text: "Sign up") }
+        it { should have_content(str) }
+      end
+
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
       end
     end
 
     describe "with valid information" do
-      before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "example@user.com"
-        fill_in "Password",     with: "foobar"
-        fill_in "Confirmation", with: "foobar"
+      describe "increases User count by one" do
+        before do
+            fill_in "Name",         with: "Example User"
+            fill_in "Email",        with: "example@user.com"
+            fill_in "Password",     with: "foobar"
+            fill_in "Confirmation", with: "foobar"
+        end
+
+          it "should create and save user and go to user profile" do
+            expect { click_button submit }.to change(User, :count).by(1)
+          end
       end
-      it "should create a user" do
-        expect { click_button submit }.to change(User, :count).by(1)
+
+      describe "creates new user and goes to profile" do
+        before do
+          fill_in "Name",         with: "Example User"
+          fill_in "Email",        with: "ExamPLe@user.Com"
+          fill_in "Password",     with: "foobar"
+          fill_in "Confirmation", with: "foobar"
+          click_button submit
+        end
+      
+        let(:user) { User.find_by_email("example@user.com") }
+        it { should have_selector("title", text: user.name) }
+        it { should have_selector("div.alert.alert-success", text: "Welcome") }
       end
     end
   end
